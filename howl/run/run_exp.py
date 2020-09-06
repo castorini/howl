b715ad65d7869799b38f28cb600ed11ea8571a64
Brandon Lee <ljj7975@gmail.com>
2020-09-06 10:05:12 -0400

    exp for hey_ff

--- howl/run/run_exp.py
+++ howl/run/run_exp.py
@@ -8,6 +8,10 @@ from datetime import datetime
 from openpyxl import Workbook, load_workbook
 
 
+def get_col(char, ind):
+    return chr(ord(char) + ind)
+
+
 def main():
     apb = ArgumentParserBuilder()
     apb.add_options(opt('--n',
@@ -16,125 +20,151 @@ def main():
                         help='number of experiments to run'),
                     opt('--dataset_path',
                         type=str,
-                        default="/data/kws/gsc/v1"))
+                        default="/data/speaker-id-split-medium"),
+                    opt('--noiseset_path',
+                        type=str,
+                        default="/data/MS-SNSD"))
 
     args = apb.parser.parse_args()
 
     # Create new workbook
     wb = Workbook()
     now = datetime.now()
-    dev_sheet = wb.create_sheet('dev', 0)
-    test_sheet = wb.create_sheet('test', 1)
+    clean_sheet = wb.create_sheet('clean', 0)
+    noisy_sheet = wb.create_sheet('noisy', 1)
 
     model_types = ['res8', 'las', 'lstm', 'mobilenet']
     col_map = {
-        'res8': 'B',
-        'las': 'C',
-        'lstm': 'D',
-        'mobilenet': 'E'
+        'Dev positive': 'B',
+        'Dev noisy positive': 'B',
+        'Dev negative': 'H',
+        'Dev noisy negative': 'H',
+        'Test positive': 'N',
+        'Test noisy positive': 'N',
+        'Test negative': 'T',
+        'Test noisy negative': 'T'
     }
-    dev_sheet['A2'] = "mean"
-    dev_sheet['A3'] = "std"
-    dev_sheet['A4'] = "p90"
-    dev_sheet['A5'] = "p95"
-    dev_sheet['A6'] = "p99"
+    metrics = ["threshold", "tp", "tn", "fp", "fn"]
+
+    clean_col_names = ["Dev positive", "Dev negative", "Test positive", "Test negative"]
+    noisy_col_names = ["Dev noisy positive", "Dev noisy negative", "Test noisy positive", "Test noisy negative"]
+
+    clean_cols_aggregated = {}
+    noisy_cols_aggregated = {}
+    
+    for col in clean_col_names:
+        ind = col_map[col] + '1'
+        clean_sheet[ind] = col
+
+        for i, metric in enumerate(metrics):
+            ind = get_col(col_map[col], i) + '2'
+            clean_sheet[ind] = metric
+
+            clean_cols_aggregated[get_col(col_map[col], i)] = []
+
+    for col in noisy_col_names:
+        ind = col_map[col] + '1'
+        noisy_sheet[ind] = col
+
+        for i, metric in enumerate(metrics):
+            ind = get_col(col_map[col], i) + '2'
+            noisy_sheet[ind] = metric
+
+            noisy_cols_aggregated[get_col(col_map[col], i)] = []
 
 
-    dev_sheet['B1'] = model_types[0]
-    dev_sheet['C1'] = model_types[1]
-    dev_sheet['D1'] = model_types[2]
-    dev_sheet['E1'] = model_types[3]
 
-    test_sheet['B1'] = model_types[0]
-    test_sheet['C1'] = model_types[1]
-    test_sheet['D1'] = model_types[2]
-    test_sheet['E1'] = model_types[3]
+    clean_sheet['A3'] = "mean"
+    clean_sheet['A4'] = "std"
+    clean_sheet['A5'] = "p90"
+    clean_sheet['A6'] = "p95"
+    clean_sheet['A7'] = "p99"
+    clean_sheet['A8'] = "sum"
+
+    noisy_sheet['A3'] = "mean"
+    noisy_sheet['A4'] = "std"
+    noisy_sheet['A5'] = "p90"
+    noisy_sheet['A6'] = "p95"
+    noisy_sheet['A7'] = "p99"
+    noisy_sheet['A8'] = "sum"
+
 
     os.system("mkdir -p exp_results")
     dt_string = now.strftime("%b-%d-%H-%M")
-    file_name = "exp_results/" + dt_string + ".xlsx"
+    file_name = "exp_results/hey_ff_" + dt_string + ".xlsx"
     print("exp_result stored at ", file_name)
 
 
     os.environ["DATASET_PATH"] = args.dataset_path
-    os.environ["BATCH_SIZE"] = "64"
-    os.environ["MAX_WINDOW_SIZE_SECONDS"] = "1"
-    os.environ["USE_NOISE_DATASET"] = "False"
-    os.environ["INFERENCE_SEQUENCE"] = "[0]"
-
     os.environ["WEIGHT_DECAY"] = "0.00001"
-    os.environ["NUM_EPOCHS"] = "20"
-    os.environ["LR_DECAY"] = "0.8"
+    os.environ["NUM_EPOCHS"] = "300"
+    os.environ["LEARNING_RATE"] = "0.001"
+    os.environ["LR_DECAY"] = "0.98"
+    os.environ["BATCH_SIZE"] = "16"
+    os.environ["MAX_WINDOW_SIZE_SECONDS"] = "0.5"
+    os.environ["USE_NOISE_DATASET"] = "True"
     os.environ["NUM_MELS"] = "40"
+    os.environ["INFERENCE_SEQUENCE"] = "[0,1,2]"
 
-    vocabs = [ name for name in os.listdir(args.dataset_path) if os.path.isdir(os.path.join(args.dataset_path, name)) ]
-    vocabs.remove('_background_noise_')
+    os.environ["VOCAB"] = '[" hey","fire","fox"]'
+    os.environ["NOISE_DATASET_PATH"] = args.noiseset_path
+    os.environ["INFERENCE_THRESHOLD"] = "0"
 
-    results = {
-        'res8': [[],[]],
-        'las': [[],[]],
-        'lstm': [[],[]],
-        'mobilenet': [[],[]],
-    }
 
     for i in range(args.n):
-        selected_vocab = random.sample(vocabs, 10)
-        print("\titeration: ", i )
-        print("\tvocabs: ", selected_vocab)
-        os.environ["VOCAB"] = str(selected_vocab).replace(" ", "").replace("\'", "\"")
+        print("\titeration: ", i , " - ", datetime.now().strftime("%H-%M"), flush=True)
         os.environ["SEED"] = str(random.randint(1,1000000))
 
-        row_index = str(i + 8)
-        dev_sheet['A'+row_index] = str(i)
-        test_sheet['A'+row_index] = str(i)
+        row_index = str(i + 10)
+        clean_sheet['A'+row_index] = str(i)
+        noisy_sheet['A'+row_index] = str(i)
 
-        for model_type in model_types:
-            print("\tmodel: ", model_type, " - ", datetime.now().strftime("%H-%M"), flush=True)
+        workspace_path = os.getcwd() + "/workspaces/exp_hey_ff_res8/" + str(i)
+        os.system("mkdir -p " + workspace_path)
 
-            if model_type == 'res8':
-                os.environ["LEARNING_RATE"] = "0.01"
-            else:
-                os.environ["LEARNING_RATE"] = "0.001"
+        exp_execution = os.system("python -m howl.run.train --model res8 --workspace " + workspace_path + "  -i " + args.dataset_path)
 
-            workspace_path = os.getcwd() + "/workspaces/" + model_type + "/" + str(i)
-            log_path =  workspace_path + "/exp.log"
+        log_path = workspace_path + "/results.csv"
+        raw_log = subprocess.check_output(['tail', '-n', '8', log_path]).decode("utf-8") 
+        logs = raw_log.split('\n')
 
-            os.system("mkdir -p " + workspace_path)
-            log_path =  workspace_path + "/exp.log"
-            exp_execution = os.system("touch " + log_path)
-            exp_execution = os.system("python -m howl.run.pretrain_gsc --model " + model_type + " --workspace " + workspace_path + " 2>&1 | tee " + log_path)
+        for log in logs:
+            if len(log) == 0:
+                break
+            vals = log.split(',')
+            key = vals[0]
+            start_col = col_map[key]
 
-            raw_log = subprocess.check_output(['cat', log_path]).decode("utf-8") 
-            logs = raw_log.split('\n')
+            for i, metric in enumerate(vals[1:]):
+                ind = get_col(start_col, i) + str(row_index)
 
-            dev_acc, test_acc = logs[-3:-1]
-            dev_acc = float(dev_acc.split(' ')[2])
-            test_acc = float(test_acc.split(' ')[2])
+                if "noisy" in key:
+                    noisy_sheet[ind] = metric
+                    noisy_cols_aggregated[get_col(start_col, i)].append(float(metric))
+                else:
+                    clean_sheet[ind] = metric
+                    clean_cols_aggregated[get_col(start_col, i)].append(float(metric))
 
-            index = col_map[model_type] + row_index
+        for idx, agg_results in clean_cols_aggregated.items():
+            results = np.array(agg_results)
+            clean_sheet[idx + '3'] = str(results.mean())
+            clean_sheet[idx + '4'] = str(results.std())
+            clean_sheet[idx + '5'] = str(np.percentile(results, 90))
+            clean_sheet[idx + '6'] = str(np.percentile(results, 95))
+            clean_sheet[idx + '7'] = str(np.percentile(results, 99))
+            clean_sheet[idx + '8'] = str(results.sum())
 
-            dev_sheet[index] = dev_acc
-            test_sheet[index] = test_acc
+        for idx, agg_results in noisy_cols_aggregated.items():
+            results = np.array(agg_results)
+            noisy_sheet[idx + '3'] = str(results.mean())
+            noisy_sheet[idx + '4'] = str(results.std())
+            noisy_sheet[idx + '5'] = str(np.percentile(results, 90))
+            noisy_sheet[idx + '6'] = str(np.percentile(results, 95))
+            noisy_sheet[idx + '7'] = str(np.percentile(results, 99))
+            noisy_sheet[idx + '8'] = str(results.sum())
 
-            results[model_type][0].append(dev_acc)
-            results[model_type][1].append(test_acc)
-
-            dev_metrics = np.array(results[model_type][0])
-            dev_sheet[col_map[model_type] + '2'] = str(dev_metrics.mean())
-            dev_sheet[col_map[model_type] + '3'] = str(dev_metrics.std())
-            dev_sheet[col_map[model_type] + '4'] = str(np.percentile(dev_metrics, 90))
-            dev_sheet[col_map[model_type] + '5'] = str(np.percentile(dev_metrics, 95))
-            dev_sheet[col_map[model_type] + '6'] = str(np.percentile(dev_metrics, 99))
-
-            test_metrics = np.array(results[model_type][1])
-            test_sheet[col_map[model_type] + '2'] = str(test_metrics.mean())
-            test_sheet[col_map[model_type] + '3'] = str(test_metrics.std())
-            test_sheet[col_map[model_type] + '4'] = str(np.percentile(test_metrics, 90))
-            test_sheet[col_map[model_type] + '5'] = str(np.percentile(test_metrics, 95))
-            test_sheet[col_map[model_type] + '6'] = str(np.percentile(test_metrics, 99))
 
         wb.save(file_name)
-
 
 
 
