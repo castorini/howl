b44380793fe51d6c7701cadb3d62fa5f9f0e055c
Brandon Lee <ljj7975@gmail.com>
2020-09-14 08:32:35 -0400

    temporary workaround

--- howl/run/create_raw_dataset.py
+++ howl/run/create_raw_dataset.py
@@ -44,21 +44,37 @@ def main():
     ctx = InferenceContext(SETTINGS.training.vocab, token_type=SETTINGS.training.token_type)
     loader = RegisteredPathDatasetLoader.find_registered_class(args.dataset_type)()
     ds_kwargs = dict(sr=SETTINGS.audio.sample_rate, mono=SETTINGS.audio.use_mono)
-    cv_train_ds, cv_dev_ds, cv_test_ds = loader.load_splits(Path(args.input_path), **ds_kwargs)
+    # cv_train_ds, cv_dev_ds, cv_test_ds = loader.load_splits(Path(args.input_path), **ds_kwargs)
 
-    if args.dataset_type == 'mozilla-cv':
-        cv_train_ds = cv_train_ds.filter(filter_fn)
-        cv_dev_ds = cv_dev_ds.filter(filter_fn)
-        cv_test_ds = cv_test_ds.filter(filter_fn)
+    # if args.dataset_type == 'mozilla-cv':
+    #     cv_train_ds = cv_train_ds.filter(filter_fn)
+    #     cv_dev_ds = cv_dev_ds.filter(filter_fn)
+    #     cv_test_ds = cv_test_ds.filter(filter_fn)
         
-    print_stats('Dataset', cv_train_ds, cv_dev_ds, cv_test_ds, skip_length=True)
+    # print_stats('Dataset', cv_train_ds, cv_dev_ds, cv_test_ds, skip_length=True)
 
-    for ds in cv_train_ds, cv_dev_ds, cv_test_ds:
-        try:
-            AudioDatasetWriter(ds).write(Path(SETTINGS.dataset.dataset_path))
-        except KeyboardInterrupt:
-            logging.info('Skipping...')
-            pass
+    # for ds in cv_train_ds, cv_dev_ds, cv_test_ds:
+    #     try:
+    #         AudioDatasetWriter(ds).write(Path(SETTINGS.dataset.dataset_path))
+    #     except KeyboardInterrupt:
+    #         logging.info('Skipping...')
+    #         pass
+
+    def train_set():
+        cv_train_ds = loader.load_train_set(Path(args.input_path), **ds_kwargs)
+        AudioDatasetWriter(cv_train_ds).write(Path(SETTINGS.dataset.dataset_path))
+
+    def dev_set():
+        cv_dev_ds = loader.load_dev_set(Path(args.input_path), **ds_kwargs)
+        AudioDatasetWriter(cv_dev_ds).write(Path(SETTINGS.dataset.dataset_path))
+
+    def test_set():
+        cv_test_ds = loader.load_test_set(Path(args.input_path), **ds_kwargs)
+        AudioDatasetWriter(cv_test_ds).write(Path(SETTINGS.dataset.dataset_path))
+
+    train_set()
+    dev_set()
+    test_set()
 
 
 if __name__ == '__main__':
