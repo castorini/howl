550dbad1ec057ef6b7eb4cee8f5264304d12625e
Edwin Zhang <edwinzhang64@gmail.com>
2021-01-24 20:44:59 -0700

    Test hubconf

--- hubconf.py
+++ hubconf.py
@@ -1,30 +1,28 @@
 """
 Configuration file for loading pretrained models using PyTorch hub
-
 Usage example: torch.hub.load("castorini/howl", "hey_fire_fox")
 """
 dependencies = ['howl', 'torch']
 
-from pathlib import Path
-from typing import Tuple
+import pathlib
+import typing
 
 import torch
 
-from howl.context import InferenceContext
-from howl.data.transform import ZmuvTransform
-from howl.model import RegisteredModel, Workspace
-from howl.model.inference import (FrameInferenceEngine, InferenceEngine,
-                                  SequenceInferenceEngine)
-from howl.settings import SETTINGS
+import howl.context as context
+import howl.data.transform as transform
+import howl.model as howl_model
+import howl.model.inference as inference
+from howl.settings import SETTINGS as _SETTINGS
 
 
-def hey_fire_fox(pretrained=True, **kwargs) -> Tuple[InferenceEngine, InferenceContext]:
+def hey_fire_fox(pretrained=True, **kwargs) -> typing.Tuple[inference.InferenceEngine, context.InferenceContext]:
     """
     Pretrained model for Firefox Voice
     """
     # TODO: Refactor into pretrained model settings class
     path = "howl-models/hey-fire-fox"
-    model = "res8"
+    model_name = "res8"
     vocab = ["hey", "fire", "fox"]
     inference_sequence = [0, 1, 2]
     max_window_size_seconds = 0.5
@@ -32,14 +30,14 @@ def hey_fire_fox(pretrained=True, **kwargs) -> Tuple[InferenceEngine, InferenceC
     use_frame = True
 
     # Set up context and workspace
-    ctx = InferenceContext(vocab, token_type=token_type,
-                           use_blank=not use_frame)
-    ws = Workspace(Path(path), delete_existing=False)
+    ctx = context.InferenceContext(
+        vocab, token_type=token_type, use_blank=not use_frame)
+    ws = howl_model.Workspace(pathlib.Path(path), delete_existing=False)
 
     # Load models
-    zmuv_transform = ZmuvTransform()
-    model = RegisteredModel.find_registered_class(model)(ctx.num_labels).eval()
-
+    zmuv_transform = transform.ZmuvTransform()
+    model = howl_model.RegisteredModel.find_registered_class(
+        model_name)(ctx.num_labels).eval()
     if pretrained:
         zmuv_transform.load_state_dict(
             torch.load(str(ws.path / 'zmuv.pt.bin')))
@@ -47,19 +45,18 @@ def hey_fire_fox(pretrained=True, **kwargs) -> Tuple[InferenceEngine, InferenceC
 
     model.streaming()
     if use_frame:
-        engine = FrameInferenceEngine(int(max_window_size_seconds * 1000),
-                                      int(SETTINGS.training.eval_stride_size_seconds * 1000),
-                                      SETTINGS.audio.sample_rate,
-                                      model,
-                                      zmuv_transform,
-                                      negative_label=ctx.negative_label,
-                                      coloring=ctx.coloring)
+        engine = inference.FrameInferenceEngine(int(max_window_size_seconds * 1000),
+                                                int(_SETTINGS.training.eval_stride_size_seconds * 1000),
+                                                _SETTINGS.audio.sample_rate,
+                                                model,
+                                                zmuv_transform,
+                                                negative_label=ctx.negative_label,
+                                                coloring=ctx.coloring)
     else:
-        engine = SequenceInferenceEngine(SETTINGS.audio.sample_rate,
-                                         model,
-                                         zmuv_transform,
-                                         negative_label=ctx.negative_label,
-                                         coloring=ctx.coloring)
-
+        engine = inference.FrameInferenceEngine(_SETTINGS.audio.sample_rate,
+                                                model,
+                                                zmuv_transform,
+                                                negative_label=ctx.negative_label,
+                                                coloring=ctx.coloring)
     engine.sequence = inference_sequence
     return engine, ctx
