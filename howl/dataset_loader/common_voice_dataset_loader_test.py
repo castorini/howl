6145a3340a9313d55fef5c28a7fa2ebd1c8f6c09
Brandon Lee <ljj7975@gmail.com>
2021-10-24 21:53:44 -0400

    fixing test case

--- howl/dataset_loader/common_voice_dataset_loader_test.py
+++ howl/dataset_loader/common_voice_dataset_loader_test.py
@@ -37,15 +37,15 @@ class TestCommonVoiceDatasetLoader(unittest.TestCase):
             train_ds, dev_ds, test_ds = dataset_loader.load_splits(sr=sample_rate)
             self.assertEqual(len(train_ds.metadata_list), 3)
             self.assertEqual(train_ds.split, DatasetSplit.TRAINING)
-            self.assertEqual(train_ds.sample_rate, sample_rate)
+            self.assertEqual(train_ds.sr, sample_rate)
 
             self.assertEqual(len(dev_ds.metadata_list), 2)
             self.assertEqual(dev_ds.split, DatasetSplit.DEV)
-            self.assertEqual(dev_ds.sample_rate, sample_rate)
+            self.assertEqual(dev_ds.sr, sample_rate)
 
             self.assertEqual(len(test_ds.metadata_list), 2)
             self.assertEqual(test_ds.split, DatasetSplit.TEST)
-            self.assertEqual(test_ds.sample_rate, sample_rate)
+            self.assertEqual(test_ds.sr, sample_rate)
 
     def test_missing_dataset(self):
         """Test failure case caused by missing dataset"""
