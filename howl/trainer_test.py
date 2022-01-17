c3a544934fa64142592b5fbcdb058323f3b5e677
Brandon Lee <ljj7975@gmail.com>
2022-01-17 18:29:38 -0500

    moving workspace outside

---
+++ howl/trainer_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from howl.config import TrainingConfig
+from howl.trainer import Trainer
+from howl.utils import test_utils
+
+
+class TrainerTest(unittest.TestCase):
+    """Test case for Trainer class"""
+
+    def test_trainer_instantiation(self):
+        """Test instantiation of Trainer"""
+        training_config_path = test_utils.test_data_path() / "test_training_config.json"
+        training_cfg = TrainingConfig.parse_file(training_config_path)
+        Trainer(training_cfg)
